60b4a54eaf317c2510ec26dd5c4221c7c2d0f8c2
Texi Schaeffer <texirae.schaeffer@gmail.com>
2022-10-24 16:34:29 -0500

    added copy and load events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './less/index.less'
 
 // Your code goes here!
+
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`);
+    const heading = document.querySelector('h1');
+    console.log(heading);
+    heading.textContent = 'READY TO GO!'
+    
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+        .then(text => {
+            console.log()
+        })
+    })
+
+}
